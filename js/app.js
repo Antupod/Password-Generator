9483fe82ce7df6b1d35e1d9f6369b28b8f644f1e
Antupod <ks2121499@gmail.com>
2022-07-02 18:50:59 +0500

    refactor

--- js/app.js
+++ js/app.js
@@ -1,45 +1,48 @@
 function generatePassword() {
-    const allSymbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!:|&+#<-_~';
+    const allSymbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!:|&+#<-_~';
     const passwordContainer = document.querySelector('.password');
-    const scrollingNumberCharacters = document.getElementById('range');
-    
+    const scrollingNumberCharacters = document.querySelector('#range');
+
+    const lettersIndex = 26;
+    const numbersIndex = 36;
+    const symbolsIndex = 46;
+
     const password = [
         {
             name: document.getElementById('uppercase'),
-            symbols: allSymbols.slice(0, 26)
+            symbols: allSymbols.slice(0, lettersIndex)
         },
         {
             name: document.getElementById('lowercase'),
-            symbols: allSymbols.slice(27, 53)
+            symbols: allSymbols.slice(0, lettersIndex).toLowerCase()
         },
         {
             name: document.getElementById('numbers'),
-            symbols: allSymbols.slice(52, 62)
+            symbols: allSymbols.slice(lettersIndex, numbersIndex)
         },
         {
             name: document.getElementById('symbols'),
-            symbols: allSymbols.slice(63, 75)
+            symbols: allSymbols.slice(numbersIndex, symbolsIndex)
         }
     ];
-    
 
     let symbolsUsed = '';
     let generatedPassword = '';
 
-    // validate checkboxes which have been selected
     for (let i = 0; i < password.length; i++) {
         if (password[i].name.checked) {
             symbolsUsed += password[i].symbols;
         }
     }
 
-    // generating a password from user-selected characters
     for (let i = 0; i < scrollingNumberCharacters.value; i++) {
         let randomNum = Math.floor(Math.random() * symbolsUsed.length);
         generatedPassword += symbolsUsed.substring(randomNum, randomNum + 1);
     }
 
-    passwordContainer.textContent = (generatedPassword == '') ? "You didn't choose anything" : generatedPassword;
+    passwordContainer.textContent = (generatedPassword === '')
+        ? "You didn't choose anything"
+        : generatedPassword;
 }
 
 document.querySelector('.button').addEventListener("click", generatePassword);
